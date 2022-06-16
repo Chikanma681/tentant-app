2c5eb8a0bca672dbf536351bbd88602d80de781a
Chikanma <chikanmaonuegbu@gmail.com>
2022-06-15 19:43:36 -0600

    installed redux

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,29 +1,12 @@
 import React from "react";
-import { Form, FormGroup, Navbar, Input, Label, Button } from "reactstrap";
 
-const Homepage = () => {
-  return (
-    <div>
-      <Navbar color="success">
-        <div style={{ color: "white" }}>
-          <h5>RentArc</h5>
-        </div>
-      </Navbar>
-      <div>
-        <Form>
-          <FormGroup>
-            <Label type="text">Email Address</Label>
-            <Input type="email" />
-          </FormGroup>
-          <FormGroup>
-            <Label type="text">Password</Label>
-            <Input type="email" />
-          </FormGroup>
-          <Button type="submit">Login</Button>
-        </Form>
-      </div>
-    </div>
-  );
+const HomePage = () => {
+
 };
 
-export default Homepage;
+export default HomePage;
+
+
+
+// let see how we gonna tackle this problem
+
