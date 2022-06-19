40b28a67cef72bd98c8aa3bcbacb8f92bd7a41da
Chikanma <chikanmaonuegbu@gmail.com>
2022-06-19 12:01:29 -0600

    integrate requests with redux

--- client/src/redux/actions/apartmentAction.js
+++ client/src/redux/actions/apartmentAction.js
@@ -1,30 +1,51 @@
 import { ApartmentTypes } from "../types/apartment";
+import axios from "axios";
+const { GET_ITEM, ADD_ITEM, DELETE_ITEM } = ApartmentTypes;
 
-export const getItem = (item) => {
-  return {
-    type: ApartmentTypes.GET_ITEM,
-    payload: item,
-  };
+export const getItem = (dispatch) => {
+  axios
+    .get("/")
+    .then((res) => {
+      dispatch({
+        type: GET_ITEM,
+        payload: res.data,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
-export const addItem = (item) => {
-  return {
-    type: ApartmentTypes.ADD_ITEM,
-    payload: item,
-  };
+export const getItemId = (id) => (dispatch) => {
+  axios
+    .get(`/${id}`)
+    .then((res) => {
+      dispatch({
+        type: GET_ITEM,
+        payload: res.data,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
-export const deleteItem = (item) => {
-    return {
-      type: ApartmentTypes.DELETE_ITEM,
-      payload: item,
-    };
-  };
-  
-export const updateItem = (item) => {
-    return {
-      type: ApartmentTypes.UPDATE_ITEM,
-      payload: item,
-    };
-  };
-  
+export const addItem = (data) => (dispatch) => {
+  axios
+    .post("/", data)
+    .then((res) =>
+      dispatch({
+        type: ADD_ITEM,
+        payload: res.data,
+      })
+    )
+    .catch((err) => console.log(err));
+};
+
+export const deleteItem = (id) => (dispatch) => {
+  axios
+    .delete(`/${id}`)
+    .then((res) =>
+      dispatch({
+        type: DELETE_ITEM,
+        payload: res.data,
+      })
+    )
+    .catch((err) => console.log(err));
+};
